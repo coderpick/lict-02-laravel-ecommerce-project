bb7fad13f771d2f23ef59f3bd4daa00b667bf192
coderpick <coderpickbd@gmail.com>
2020-12-03 19:58:22 +0600

    Backend layout templating complete

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['as'=>'admin.','prefix'=>'admin/','namespace'=>'Admin','middleware'=>['auth']],function(){
+    Route::get('dashboard',[DashboardController::class,'index'])->name('dashboard');
+});
